d110912333e5f14ffd3fd0e60c7b9124f2fec861
yumunull <yumunull@gmail.com>
2025-04-18 23:52:10 +0800

    share and download function

--- app/_component/ImagePreviewer.tsx
+++ app/_component/ImagePreviewer.tsx
@@ -1,7 +1,7 @@
 ﻿"use client"
-import React, {FC, useEffect, useState} from "react";
+import React, {FC, useEffect, useRef, useState} from "react";
 import Image from "next/image";
-import {useRouter} from "next/navigation";
+import {usePathname, useRouter} from "next/navigation";
 
 interface Props {
     id: string
@@ -19,6 +19,35 @@ const ImagePreviewer: FC<Props> = ({id}) => {
             setImageUrl(url)
         })()
     }, [id])
+    
+    const handleCopyUrl = async () => {
+        if (!imageUrl) return
+        await navigator.clipboard.writeText(`${process.env.NEXT_PUBLIC_BASE_URL}/${id}`)
+    }
+    
+    const handleDownload = async () => {
+        if (!imageUrl) return
+        try {
+            // 获取文件
+            const response = await fetch(imageUrl);
+            const blob = await response.blob();
+
+            const blobUrl = window.URL.createObjectURL(blob);
+
+            const link = document.createElement('a');
+            link.href = blobUrl;
+            link.download = id; // 设置下载的文件名
+            document.body.appendChild(link);
+            link.click();
+
+            document.body.removeChild(link);
+            window.URL.revokeObjectURL(blobUrl);
+        } catch (error) {
+            console.error('下载失败:', error);
+        }
+
+    }
+    
     return (
         <>
             <div className={`card h-[360px] mx-auto`}>
@@ -29,16 +58,17 @@ const ImagePreviewer: FC<Props> = ({id}) => {
 
             <div className={`flex justify-center gap-x-4 mt-8`}>
                 <button
+                    onClick={handleCopyUrl}
                     className={`bg-sky-blue px-4 py-2 rounded-md flex text-xs items-center gap-x-2 hover:cursor-pointer text-white`}>
                     <Image src={`/Link.svg`} alt={`share icon`} width={16} height={16}/>
                     <span>Share</span>
                 </button>
                 <button
+                    onClick={handleDownload}
                     className={`bg-sky-blue px-4 py-2 rounded-md flex text-xs items-center gap-x-2 hover:cursor-pointer text-white`}>
                     <Image src={`/download.svg`} alt={`download icon`} width={16} height={16}/>
                     <span>Download</span>
                 </button>
-
             </div>
         </>
     )
